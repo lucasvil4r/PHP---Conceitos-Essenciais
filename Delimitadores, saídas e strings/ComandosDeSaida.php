8a05f106d622b900ab671bac6bab9a750e5b987a
lucasvil4r <90861563+lucasvil4r@users.noreply.github.com>
2022-06-28 21:28:53 -0300

    Comandos de saida PHP

---
+++ Delimitadores, saídas e strings/ComandosDeSaida.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<title>Untitled</title>
+<meta charset="utf-8">
+</head>
+<body>
+
+<?php 
+echo "comando de saida echo";
+echo "<br>";
+echo ("comando de saida echo");
+echo "<hr>";
+print ("comando de saida print");
+echo "<br>";
+print "comando de saida print";
+// pesquisar php.net print
+
+//$ret = echo "Hello World";
+?>
+
+<hr>
+<?= "eu sou o reduzido do echo" ?>
+
+</body>
+</html>
