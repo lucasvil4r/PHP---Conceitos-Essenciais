c228c0e72dc6987256282d432b329a4e13982b58
lucasvil4r <lucasvilar-celestino@hotmail.com>
2022-07-01 14:45:00 -0300

    Declarando variaveis

---
+++ Delimitadores, saídas e strings/Variaveis.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ "Definição"
+ "espaço na memória ao qual eu me referencio através de um endereço<br>"
+ "locais usados para armazenar informações durante a execução do programa"
+
+Regras
+1 - Sempre antecedidas pelo caracter "$"(dolar)
+2 - Começa com uma letra ou o simbolo "_"
+3 - Não podem conter espaços
+4 - Sensitive Case - $NOME <> $nome <> $Nome
+5 - Validos - $USER ; $login; $_cidade ; $_UF;";
+6 - não válidos - $2mes; $#dia  (nao começa com number ou com caracteres especiais)
+*/
+//iniciando variavel
+$a = "impacta";
+echo $a;
+
+?>
