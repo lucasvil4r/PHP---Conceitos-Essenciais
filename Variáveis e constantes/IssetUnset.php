4037c1dabcc1c946c2b85a63dd7ebc382ac68fa8
lucasvil4r <lucasvilar-celestino@hotmail.com>
2022-07-12 16:32:16 -0300

    Update

---
+++ Variáveis e constantes/IssetUnset.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Isset / Unset</title>
+</head>
+<body>
+    
+<?php
+
+//Estudo de insset - testa a existencia da variavel
+
+$a = 12;
+
+echo "A \$a existe:";
+echo isset($a); // essa variavel existe 1 -> true
+echo "<hr>";
+
+echo "A \$b existe:";
+echo isset($b); //essa variavel nao existe 0 -> NULL
+echo "<hr>";
+
+echo "A \$b nao existe:";
+echo !isset($a); //"!" => negacao inverte valor do teste logico
+echo "<hr>";
+
+echo "A \$b nao existe:";
+echo !isset($b); //"!" => negacao inverte valor do teste logico
+echo "<hr>";
+
+?>
+
+</body>
+</html>
