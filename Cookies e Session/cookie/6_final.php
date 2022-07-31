0d74e88380bc0ebe1bd71daaf559ece88b075ccf
lucasvil4r <90861563+lucasvil4r@users.noreply.github.com>
2022-07-30 21:44:38 -0300

    Cookies e Session

---
+++ Cookies e Session/cookie/6_final.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<title></title>
+<meta charset="utf-8">
+</head>
+<body>
+<?php 
+###########################################################3
+print "<hr>";
+#####################################33
+print "Esta é a Cookie: <br>";
+print "<pre>";
+print_r($_COOKIE);
+print "</pre>";
+print "<hr>";
+?>
+
+</body>
+</html>
