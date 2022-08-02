06e89e1ff90c02c902342557a18cc6832a85924d
lucasvil4r <90861563+lucasvil4r@users.noreply.github.com>
2022-08-02 18:21:36 -0300

    Include or require

---
+++ Inclusão de arquivo/1_inclusao.php
@@ -0,0 +1,18 @@
+<?php 
+include_once 'function.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php @include 'listasss.php' ;
+echo teste();
+?>
+<hr>
+<?php include 'lista.php' ?>
+<hr>
+<?php include 'lista.php' ?>
+</body>
+</html>
---
+++ Inclusão de arquivo/2_require.php
@@ -0,0 +1,24 @@
+<?php 
+require_once 'function.php';
+require_once 'function.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+echo teste(); 
+
+@require 'lista.php' ;
+
+echo teste();
+?>
+<hr>
+<?php require 'lista.php' ?>
+<hr>
+<?php require 'lista.php' ?>
+</body>
+</html>
