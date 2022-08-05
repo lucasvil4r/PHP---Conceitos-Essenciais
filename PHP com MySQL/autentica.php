562a22d9095fc923d276dcc1d2ad2af7049f2726
lucasvil4r <90861563+lucasvil4r@users.noreply.github.com>
2022-08-05 15:21:16 -0300

    Finish curso

--- PHP com MySQL/autentica.php
+++ PHP com MySQL/autentica.php
@@ -1,19 +1,19 @@
 <?php
 
-
 if(isset($_POST['autenticar'])){
 	include 'inc/conecta.php';
 	$sql="select * from usuarios where usuario = ? AND senha = ?";
-	$stm = $conn->prepare($sql);
+	$stm = $conn->prepare($sql);//prepara consulta
 	extract($_POST);
-	$stm->bind_param('ss',$login,$senha);
-	$stm->execute();
-	$stm->store_result();
+	$stm->bind_param('ss',$login,$senha);//"2 strings, var login, var senha tipa as var"
+	$stm->execute();//executa query
+	$stm->store_result();//
 	
+	// user altenticado = retorne true
 	if($stm->num_rows == 1){
 		session_start();
-		$_SESSION['logado']=true;
-		$_SESSION['usuario']=$_POST['login'];
+		$_SESSION['logado']= true;//autenticou
+		$_SESSION['usuario']= $_POST['login']; //Passou nome de user
 
 		if(isset($_POST['lembrar'])){
 			setcookie('login',$_POST['login'],time()+10000);
@@ -27,11 +27,9 @@ if(isset($_POST['autenticar'])){
 
 		header("Location:listar.php");
 	}else{
-		header("Location:index.php?aviso=tentou");
+		header("Location:index.php?aviso=tentou");// usuario não foi autenticado
 	}
 	exit;
 }else{
 	header("Location:index.php?aviso=url");
 }
-
-
